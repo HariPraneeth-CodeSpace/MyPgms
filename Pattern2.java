935297fbe5b461ce40571f17c7f4d816af4f977b
HariPraneeth <155291750+hari8122004@users.noreply.github.com>
2025-01-24 15:26:03 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,12 @@
+package Pgm;
+
+public class Pattern2 {
+	public static void main(String[] args) {
+				for(int r=5;r>=1;r--) {// 
+			for(int c=5;c>=r;c--) {// 
+				System.out.print(c+" ");// 
+			}
+			System.out.println();
+		}
+	}
+}
